3e39f63fbb825e55a77b65e6a5f0b237d0adb613
nokonoko1203 <healtheworld.1203@gmail.com>
2025-05-10 01:06:47 +0900

    fix

--- spatial-id-py/tests/test_generate_id.py
+++ spatial-id-py/tests/test_generate_id.py
@@ -1,5 +1,6 @@
 from spatial_id_py import generate_spatial_id
 
 def test_generate_spatial_id():
-    assert generate_spatial_id(35.0, 135.0, 0.0, 25).startswith("/25/")
-    assert isinstance(generate_spatial_id(0.0, 0.0, 0.0, 25), str)
+    assert generate_spatial_id(0.0, 0.0, 10.0, 25).startswith("/25/")
+    assert isinstance(generate_spatial_id(0.0, 0.0, 10.0, 25), str)
+    print(generate_spatial_id(0.0, 0.0, 10.0, 25))
